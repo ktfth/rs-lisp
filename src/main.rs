b58bfb7be3c84e47139f66a204e227d31fe91c8a
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-03-15 23:20:39 -0300

    Prepared to read a file and bootstraped run function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use std::env;
 use std::process;
 
-fn run_file(file: &str) {
-    println!("run file: {:?}", file);
+fn run (data: String) {
+    println!("{}", data);
+}
+
+fn run_file(file: &str, had_error: bool) {
+    let data = std::fs::read_to_string(file).unwrap();
+    run(data);
+    if had_error {
+        process::exit(65);
+    }
 }
 
 fn run_prompt() {
@@ -13,11 +21,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let params = args.iter().skip(1).collect::<Vec<_>>();
 
+    let had_error = false;
+
     if params.len() > 1 {
         println!("Usage: rs-lisp [script]");
         process::exit(1);
     } else if params.len() == 1 {
-        run_file(params[0]);
+        run_file(params[0], had_error);
     } else {
         run_prompt();
     }
