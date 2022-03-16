f3e265dfb94e8b537125c77c63e538f9aa7264c6
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-03-15 22:41:59 -0300

    Bare structure of entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::process;
+
+fn run_file(file: &str) {
+    println!("run file: {:?}", file);
+}
+
+fn run_prompt() {
+    println!("run prompt");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let params = args.iter().skip(1).collect::<Vec<_>>();
+
+    if params.len() > 1 {
+        println!("Usage: rs-lisp [script]");
+        process::exit(1);
+    } else if params.len() == 1 {
+        run_file(params[0]);
+    } else {
+        run_prompt();
+    }
 }
